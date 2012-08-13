49257e72617ef3f7e966f36ac4fd01894803e2aa
Luke Daley <ld@ldaley.com>
2012-08-13 19:59:36 +0100

    Some improvements to the first demo.

--- talks/intro/src/demos/01-hello-world/src/main/java/pkg/Main.java
+++ talks/intro/src/demos/01-hello-world/src/main/java/pkg/Main.java
@@ -2,9 +2,12 @@ package pkg;
 
 import java.io.PrintWriter;
 
+import com.google.common.collect.Sets;
+
 public class Main {
     
     public static void main(String[] args) {
+        Sets.newHashSet();
         new HelloWorld().say(new PrintWriter(System.out));
     }
     
